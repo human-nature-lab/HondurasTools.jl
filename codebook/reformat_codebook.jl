fceac884de539d6f0cb51546c7b373b7f12ffb62
Eric Martin Feltham <eric.feltham@yale.edu>
2022-10-21 18:02:45 -0400

    codebook comments and cleaning

--- codebook/reformat_codebook.jl
+++ codebook/reformat_codebook.jl
@@ -1,52 +1,30 @@
 # reformat_codebook.jl
-# reform the separate wave codebooks into a single, somewhat readable
-# table.
-# writes a csv and a jld object
-# needed: csv versions of the existing codebooks (take the Liza-format excel
-# tables and convert the first sheet to a csv file)
-# this script is also a great way to check for oddities in the data (e.g.,)
-# repeated rows where there shouldn't be.
-
-using DataFrames, DataFramesMeta, CSV
-
-import JLD2.save_object
-
-####
-
-# more logically re-nest the dataframe
-function nestdf(x)
-    x = @chain x begin
-        groupby(:variable_name)
-        combine(
-            :repeated_question => Ref∘unique => :repeated_question,
-            :question_type => Ref∘unique => :question_type,
-            :survey => Ref∘unique => :survey,
-            :question_english_women => Ref∘unique => :question_english_women,
-            :question_english_men => Ref∘unique => :question_english_men,
-            :option_code => Ref∘unique => :option_code,
-            :option_english_women => Ref∘unique => :option_english_women,
-            :option_english_men => Ref∘unique => :option_english_men,
-        )
-    end
-    
-    for (i,c) in enumerate(eachcol(x))
-        if unique(length.(c)) == [1]
-            x[!, i] = reduce(vcat, c)
-        end
-    end
-
-    return x
-end
-
-# combine a vector of strings
-function together(X)
-    return [join(e, ", ") for e in X];
-end
-
-####
 
 #=
-note that these are the csv files generated from the "_vx" version of the 
+reform the separate wave codebooks into a single, somewhat readable
+table.
+
+WRITE a csv and a jld object
+
+INPUT csv versions of the existing codebooks (take the Liza-format excel
+tables and convert the first sheet to a csv file)
+
+This script is also a great way to check for oddities in the data (e.g.,
+repeated rows where there shouldn't be.)
+
+N.B.
+- integer values in rows seem sorted as if they are a string (e.g.,
+["1", "10", "9"]). maybe fix this someday. double check if it just matches
+the order of the english options column.
+=#
+
+using DataFrames, DataFramesMeta, CSV
+import JLD2.save_object
+
+include("reformat_functions.jl")
+
+#=
+Note that these are the csv files generated from the "_vx" version of the 
 usual codebooks.
 
 additionally, on 2022-10-21, I made a few corrections to the data directly
@@ -97,29 +75,21 @@ x1 = nestdf(x1)
 x2 = nestdf(x2)
 x3 = nestdf(x3)
 
-describe(x1)
-describe(x2)
-describe(x3)
-
-x2[findall(length.(x2.repeated_question) .> 1), :]
-
-x2[findall(length.(x2.question_english_men) .> 1), :]
-
-x3[findall(length.(x3.question_english_men) .> 1), :]
+# useful checks
+# describe(x1)
+# x2[findall(length.(x2.repeated_question) .> 1), :]
+# x2[findall(length.(x2.question_english_men) .> 1), :]
+# x3[findall(length.(x3.question_english_men) .> 1), :]
 
 x = vcat(x1, x2, x3)
 
-names(x)
-
+## WRITE
 save_object("honduras_codebook.jld2", x) # before re-nesting
-
-names(x)
 
 x = nestdf(x)
 
-describe(x)
-names(x)
-
+# for columns with multiple values per cell, make into a comma separated
+# string that can be read easily by a human.
 x.survey = together(x.survey);
 x.repeated_question = together(x.repeated_question);
 # x.question_english_men = together(x.question_english_men); # not needed
@@ -129,4 +99,5 @@ x.option_code = together(x.option_code);
 x.option_english_men = together(x.option_english_men);
 x.option_english_women = together(x.option_english_women);
 
+## WRITE
 CSV.write("reformatted_codebook.csv", x; sep = ";")
---
+++ codebook/reformat_functions.jl
@@ -0,0 +1,40 @@
+# reformat_functions.jl
+# deps in reformat_codebook.jl
+
+"""
+        nestdf(x)
+
+More logically re-nest the dataframe.
+"""
+function nestdf(x)
+    x = @chain x begin
+        groupby(:variable_name)
+        combine(
+            :repeated_question => Ref∘unique => :repeated_question,
+            :question_type => Ref∘unique => :question_type,
+            :survey => Ref∘unique => :survey,
+            :question_english_women => Ref∘unique => :question_english_women,
+            :question_english_men => Ref∘unique => :question_english_men,
+            :option_code => Ref∘unique => :option_code,
+            :option_english_women => Ref∘unique => :option_english_women,
+            :option_english_men => Ref∘unique => :option_english_men,
+        )
+    end
+    
+    for (i,c) in enumerate(eachcol(x))
+        if unique(length.(c)) == [1]
+            x[!, i] = reduce(vcat, c)
+        end
+    end
+
+    return x
+end
+
+"""
+        together(X)
+
+Combine a vector of strings and comma-separate.
+"""
+function together(X)
+    return [join(e, ", ") for e in X];
+end
