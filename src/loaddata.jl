566f65fc5d3b7e8c895fcd977e52ceab92048b77
Eric Martin Feltham <eric.feltham@yale.edu>
2022-10-24 16:56:59 -0400

    fix

--- src/loaddata.jl
+++ src/loaddata.jl
@@ -11,5 +11,5 @@ data("ds1_Krackhardt")
 """
 function load_mbvillages()
     dd = joinpath(dirname(@__FILE__), "..", "codebook")
-    return read(dd * "/microbiome_villages.csv", DataFrame)
+    return CSV.read(dd * "/microbiome_villages.csv", DataFrame)
 end
