c21d88e3782b5d02e229c9c2a603ca6fc51ab699
Eric Martin Feltham <eric.feltham@yale.edu>
2023-07-02 18:00:34 -0400

    improved processing, for wealth index calc

--- src/clean_household.jl
+++ src/clean_household.jl
@@ -1,7 +1,7 @@
 # clean_household.jl
 
 """
-        clean_household(hh)
+        clean_household(hh, waves; nokeymiss = true)
 
 Clean the household level data. `hh` must be a vector of dataframes. Data must Must be ordered by and match `waves`.
 
@@ -10,6 +10,7 @@ ARGS
 
 - resp: a vector of DataFrames, with entries for each wave of the data.
 - waves: indicate the wave of each DataFrame in the same order as resp.
+- nokeymiss = true : whether to filter to not missing on key variables: village code and building id
 
 """
 function clean_household(hh::Vector{DataFrame}, waves; nokeymiss = true)
@@ -59,43 +60,78 @@ function clean_household(hh::Vector{DataFrame}, waves; nokeymiss = true)
         rename!(hh, :household_wealth_index => :hh_wealth);
     end
 
-    if :l0400 ∈ hh_desc.variable
-        rename!(hh, :l0400 => :watersource);
-        hh.watersource = categorical(hh.watersource);
-    end
+    xs = [
+        (:l0100 => :children_under12, false, missing),
+        (:l0200 => :girls_under12, false, missing),
+        (:l0300 => :boys_under12, false, missing),
+        (:l0400 => :watersource, false, ["Dont_Know" => "Don't Know"]),
+        (:l0600 => :cleaningagent, true, ["Dont_Know" => "Don't Know"]),
+        (:l0700 => :toilettype, true, ["Dont_Know" => "Don't Know"]),
+        (:l0800 => :toiletshared, true, ["Dont_Know" => "Don't Know"]),
+        (
+            :l0900a => :electricity, true,
+            ["Electricity" => "Yes", missing => "No"]
+        ),
+        (
+            :l0900b => :radio, true,
+            ["Radio" => "Yes", missing => "No"]
+        ),
+        (
+            :l0900c => :tv, true,
+            ["Television" => "Yes", missing => "No"]
+        ),
+        (
+            :l0900d => :cell, true,
+            ["Cell/mobile phone" => "Yes", missing => "No"]
+        ),
+        (
+            :l0900e => :landline, true,
+            ["Non-mobile phone" => "Yes", missing => "No"]
+        ),
+        (:l0900f => :fridge, true, ["Refrigerator" => "Yes", missing => "No"]),
+        (:l1000 => :cooktype, true, ["Dont_Know" => "Don't Know"]),
+        (:l1100 => :cookfueltype, true, ["Dont_Know" => "Don't Know"]),
+        (:l1200 => :kitchen, true, ["Dont_Know" => "Don't Know"]),
+        (:l1300 => :flooring, true, ["Dont_Know" => "Don't Know"]),
+        (:l1400 => :windows, true, ["Dont_Know" => "Don't Know"]),
+        (:l1500 => :walltype, true, ["Dont_Know" => "Don't Know"]),
+        (:l1600 => :roofing, true, ["Dont_Know" => "Don't Know"]),
+        (
+            :l0900g => :noneof, true,
+            ["None of the above" => "Yes", missing => "No"]
+        )
+    ];
+    
+    for (x, t, c) in xs
+        if x ∈ hh_desc.variable
+            rename!(hh, x)
+            if t
+                hh[!, x] = categorical(hh[!, x]);
+                if !ismissing(c)
+                    recode!(hh[!, x] = c...)
+                end
 
-    if :l0900a ∈ hh_desc.variable
-        rename!(hh, :l0900a => :elec);
-        hh.elec = categorical(hh.elec);
-        recode!(hh.elec, "Electricity" => "Yes", missing => "No");
-    end
+                # if really binary make boolean
+                tst = sort(collect(skipmissing(unique(hh[!, x]))))
+                if tst == ["No", "Yes"]
+                    xv = Vector{Union{Missing, Bool}}(missing, length(hh[!, x]))
+                    for (i, e) in enumerate(hh[!, x])
+                        if !ismissing(e)
+                            xv[i] = if e == "Yes"
+                                true
+                            elseif e == "No"
+                                false
+                            end
+                        end
+                    end
+                    hh[!, x] = xv
+                end
+            end
 
-    # rename!(h3, :l0900b => :radio)
-    # h3.radio = categorical(h3.radio)
-    # recode!(h3.radio, "Radio" => "Yes", missing => "No")
-
-    if :l0900c ∈ hh_desc.variable
-        rename!(hh, :l0900c => :tv);
-        hh.tv = categorical(hh.tv);
-        recode!(hh.tv, "Television" => "Yes", missing => "No");
-    end
-
-    if :l0900d ∈ hh_desc.variable
-        rename!(hh, :l0900d => :cell);
-        hh.cell = categorical(hh.cell);
-        recode!(hh.cell, "Cell/mobile phone" => "Yes", missing => "No");
-    end
-
-    if :l0900f ∈ hh_desc.variable
-        rename!(hh, :l0900f => :fridge);
-        hh.fridge = categorical(hh.fridge);
-        recode!(hh.fridge, "Refrigerator" => "Yes", missing => "No");
-    end
-
-    if :l0900g ∈ hh_desc.variable
-        rename!(hh, :l0900g => :noneof);
-        hh.noneof = categorical(hh.noneof);
-        recode!(hh.noneof, "None of the above" => "Yes", missing => "No");
+            if !any(ismissing.(hh[!, x]))
+                disallowmissing!(hh, x)
+            end
+        end
     end
 
     # remove irrelevant variables
@@ -108,6 +144,114 @@ function clean_household(hh::Vector{DataFrame}, waves; nokeymiss = true)
     # rename to avoid conflicts with other data
     rename!(hh, :survey_start => :hh_survey_start);
     rename!(hh, :new_building => :hh_new_building);
+
+    # decisions related to role of women
+    xs = [
+        (
+            :i0200 => :how_husband_earnings_spent, true,
+            ["Dont_Know" => "Don't Know"]
+        ),
+        (
+            :i0300 => :beating_wife_neglect_children, true,
+            ["Dont_Know" => "Don't Know"]
+        ),
+        (
+            :i0400 => :beating_wife_leaves_house, true,
+            ["Dont_Know" => "Don't Know"]
+        ),
+        (
+            :i0500 => :beating_wife_argues, true,
+            ["Dont_Know" => "Don't Know"]
+        ),
+        (
+            :i0600 => :beating_burns_food, true,
+            ["Dont_Know" => "Don't Know"]
+        ),
+        (
+            :i0700 => :beating_refuse_sex, true,
+            ["Dont_Know" => "Don't Know"]
+        ),
+        (
+            :i0800 => :girl_join_partner_age, false,
+            missing
+        ),
+        (
+            :i0900 => :girl_join_partner_parents_decide, true,
+            ["Dont_Know" => "Don't Know"]
+        ),
+        (
+            :i1000 => :girl_first_baby_age, false, 
+            missing
+        ),
+        (
+            :i1100 => :woman_health_decision, true,
+            ["Dont_Know" => "Don't Know"]
+        ),
+        (
+            :i1200 => :woman_when_folic_acid, true,
+            ["Dont_Know" => "Don't Know"]
+        ),
+        (
+            :i1300 => :women_pregnancy_checkups, true,
+            ["Dont_Know" => "Don't Know"]
+        ),
+        (
+            :i1300 => :women_pregnancy_checkups, false,
+            missing
+        )
+    ];
+
+    vs = [
+        :girl_join_partner_age, :girl_first_baby_age, :women_pregnancy_checkups
+    ];
+    
+    for v in vs
+        if v ∈ hh_desc.variable
+            vx = Vector{Union{Missing, Int}}(missing, length(hh[!, v]))
+            for (i, e) in enumerate(hh[!, v])
+                x = tryparse(Int, e)
+                if !isnothing(x)
+                    vx[i] = x
+                end
+            end
+            hh[!, v] = vx
+            if !any(ismissing.(hh[!, v]))
+                disallowmissing!(hh, v)
+            end
+        end
+    end
+
+    for (x, t, c) in xs
+        if x ∈ hh_desc.variable
+            rename!(hh, x)
+            if t
+                hh[!, x] = categorical(hh[!, x]);
+                if !ismissing(c)
+                    recode!(hh[!, x] = c...)
+                end
+
+                # if really binary make boolean
+                tst = sort(collect(skipmissing(unique(hh[!, x]))))
+                if tst == ["No", "Yes"]
+                    xv = Vector{Union{Missing, Bool}}(missing, length(hh[!, x]))
+                    for (i, e) in enumerate(hh[!, x])
+                        if !ismissing(e)
+                            xv[i] = if e == "Yes"
+                                true
+                            elseif e == "No"
+                                false
+                            end
+                        end
+                    end
+                    hh[!, x] = xv
+                end
+            end
+            
+            if !any(ismissing.(hh[!, x]))
+                disallowmissing!(hh, x)
+            end
+        end
+    end
 
     if nokeymiss
         dropmissing!(hh, :village_code);
