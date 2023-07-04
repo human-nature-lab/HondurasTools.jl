8bd95adaaf7c95101e9396cc3f67fad1d5b86c01
Eric Martin Feltham <eric.feltham@yale.edu>
2023-07-04 16:24:36 -0400

    make a vector boolean

--- src/utilities.jl
+++ src/utilities.jl
@@ -29,3 +29,23 @@ function misstring(x)
 end
 
 replmis(x) = ismissing(x) ? false : true
+
+boolstring(x) = return if x == "Yes"
+    true
+elseif x == "No"
+    false
+else error("not Yes/No")
+end
+
+function boolvec(vector)
+    return if nonmissingtype(eltype(vector)) <: AbstractString
+        passmissing(boolstring).(vector)
+    elseif nonmissingtype(eltype(vector)) <: Signed
+        if sort(collect(skipmissing(unique(vector)))) == [1, 2]
+            passmissing(Bool).(vector .- 1)
+        elseif sort(collect(skipmissing(unique(vector)))) == [0, 1]
+            passmissing(Bool).(vector)
+        end
+    else error("check type")
+    end
+end
