4d27db5ebc7c73c70c3daeb98bfd1b8d91f9df26
Eric Martin Feltham <eric.feltham@yale.edu>
2023-10-21 15:50:18 -0400

    add names

--- src/clean_perception.jl
+++ src/clean_perception.jl
@@ -799,6 +799,37 @@ function clean_perception!(resp)
         end
     end;
 
+    # diarrhea skipped
+    pvars = [
+        :girl_partner_moralperc, :girl_partner_good_age,
+        :girl_baby_moralperc, :girl_baby_good_age,
+        :avoid_preg_perc, :avoid_preg_ever, :avoid_preg_now, :avoid_preg,
+        :avoid_preg_moralperc,
+        :folic_perc, :folic_now, :folic_1wk, :folic_good_when,
+        :folic_good, :folic,
+        :prenatal_care_perc, :prenatal_care_any, :prenatal_care_when,
+        :prenatal_care_moralperc, :prenatal_care_good_when,
+        :homebirth_perc, :wherebirth, :birth_good_where, :homebirth_moralperc,
+        :birthdecision_perc, :birthdecision,
+        :postnatal_care_perc, :postnatal_care_any, :postnatal_care_when, 
+        :postnatal_good,
+        :baby_bath_perc, :baby_bath_moralperc, :baby_bath,
+        :baby_skin_perc, :baby_skin_moralperc, :baby_skin_good, :baby_skin,
+        :fajero_perc, :fajero, :fajero_good_1, :fajero_good_2,
+        :chupon_perc, :chupon_moralperc, :chupon,
+        :laxatives_perc, :laxatives_good,
+        :diarrhea_3wk,
+        :wash_perc, :wash_good, :avoid_smoke_perc, :avoid_smoke,
+        :father_check_perc, :father_check,
+        :father_check_moralperc, :father_check_good,
+        :father_wait_perc, :father_wait, :father_wait_moralperc, :father_wait_good,
+        :father_care_sick_perc, :father_care_sick_moralperc, :father_care_sick_good,
+        :men_hit_perc, :men_hit_moralperc, :men_hit_neglect_good,
+        :men_hit_house_good, :men_hit_argue_good, :men_hit_food_good,
+        :men_hit_sex_good,
+        :men_hit_good, 
+    ];
+
     # update variables: this will basically copy wave 3 values to wave 4
     # most of these were not likely collected in wave 4
     let wx = 4
