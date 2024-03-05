6c6a1f654a09638932209b7a5b06b93ae8b8e8e5
emfeltham <eric.feltham@yale.edu>
2024-03-04 22:23:38 -0500

    fix lines of chance/imprpvement

--- src/figures/figure_utilities.jl
+++ src/figures/figure_utilities.jl
@@ -8,7 +8,7 @@
 Add line of chance to ROC-space plot, the line L"y = x".
 """
 function chanceline!(ax)
-    lines!(ax, (1:-0.1:0.5), 0:0.1:0.5; linestyle = :solid, color = oi[6])
+    lines!(ax, (0.0:0.1:1.0), 0:0.1:1.0; linestyle = :dot, color = (:black))
 end
 
 """
@@ -24,4 +24,5 @@ This is the line L"y = 1 - x".
 function improvementline!(ax)
     # line of improvement
     lines!(ax, (0.5:-0.1:0), 0.5:0.1:1; linestyle = :solid, color = oi[3])
+    lines!(ax, (1:-0.1:0.5), 0:0.1:0.5; linestyle = :solid, color = oi[6])
 end
