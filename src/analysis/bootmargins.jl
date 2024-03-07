6a4572a09b197d9b2e7ad51e565c70d6bdd81431
emfeltham <eric.feltham@yale.edu>
2024-03-07 07:37:53 -0500

    improve j bootstrap

--- src/analysis/bootmargins.jl
+++ src/analysis/bootmargins.jl
@@ -3,135 +3,225 @@
 
 using GeometryBasics:Point
 
-function _assign_margins!(
-    mdf, mm, pbt, pbf, num_bs, blocks, vbl, kin, crt, crf, manual_typicals
-)
-    Threads.@threads for i in 1:num_bs
-        block = blocks[i]
-        # for a bimodel
-        for (e, b) in zip([mm.tpr, mm.fpr], [pbt, pbf])
+function _installparams!(bm, Θ, i, rates)
+    for r in rates
+        e = @views bm[r]
+        b, u = Θ[r]
+        
+        # install new parameters to bimodel
+        e.β = b[i]
+        e.θ = u[i];
+    end
+end
 
-            # extract parameters
-            gsβ = groupby(DataFrame(b.β), :iter);
-            gsθ = b.θ;
+function _assignmargins!(bstimates, refgrids, bm, Θ, iters, invlink, rates)
+    for i in 1:iters
+        _installparams!(bm, Θ, i, rates)
 
-            # install new parameters to mm
-            e.β = gsβ[i].β
-            e.θ = gsθ[i];
+        # marginal effect calculation
+        # overwrite -> this would have to be adjusted for Threads
+        apply_referencegrids!(
+            bm, refgrids;
+            invlink, multithreaded = false
+        )
+
+        # copy to the storage location
+        # no easy way to avoid effects! dump into a the refgrid dataframe
+        for r in rates
+            bstimates[r][i] .= refgrids[r][!, :response]
         end
-
-        # calculate marginal effects (for both models)
-        # we want tpr + fpr -> tnr = false
-        mm_marg = bimargins(
-            mm, [vbl, kin], crt, crf; tnr = false, manual_typicals
-        );
-        mm_marg.iter .= i;
-
-        # could improve speed here
-        # figure out: general case for rows and col num
-        # append!(mdf, mm_marg)
-        @assert nrow(mm_marg) == length(block)
-        @assert sunique(names(mm_marg)) == sunique(names(mdf))
-        mdf[block, :] = mm_marg[!, names(mdf)]
     end
 end
 
 """
-bootmargins(
-    vbl, mm, pbt, pbf;
-    resp_var = :response, iters = 1000, minimal = true
-)
+        bootstrapdata(pbs, iters)
+
+## Description
+
+Create a structure to easily access bootstrap coefficients.
+"""
+function bootstrapdata(pbs, iters)
+    Θ = (
+        tpr = (
+            β = Vector{Vector{Float64}}(undef, iters),
+            θ = Vector{Vector{Float64}}(undef, iters)
+        ),
+        fpr = (
+            β = Vector{Vector{Float64}}(undef, iters),
+            θ = Vector{Vector{Float64}}(undef, iters)
+        ),
+    );
+
+    gβ = (
+        tpr = groupby(DataFrame(pbs[:tpr].β), :iter),
+        fpr = groupby(DataFrame(pbs[:fpr].β), :iter)
+    );
+
+    for r in rates
+        for i in eachindex(1:iters)
+            # check name order
+            # @assert gβ[r][i][!, :coefname] ...
+            Θ[r][:β][i] = gβ[r][i].β
+            Θ[r][:θ][i] = pbs[r].θ[i]
+        end
+    end
+    return Θ
+end
+
+function _bootstrap_allocate(refgrids, iters, respvar)
+
+    bstimates = (
+        tpr = Vector{Vector{Float64}}(undef, iters),
+        fpr = Vector{Vector{Float64}}(undef, iters),
+    );
+
+    for r in rates
+        for i in eachindex(bstimates[:tpr])
+            bstimates[r][i] = similar(refgrids[:tpr][!, respvar])
+        end
+    end
+    return bstimates
+end
+
+"""
+        postprocess_bootstrap!(refgrids, bstimates, iters)
+
+## Description
+
+Rearrange and store bootstrap predictions in the refgrids.
+"""
+function postprocess_bootstrap!(refgrids, bstimates, iters)
+    for r in rates
+        refgrids[r].response_bs = Vector{Vector{Float64}}(
+            undef, nrow(refgrids[r])
+        );
+        for i in 1:nrow(refgrids[r])
+            refgrids[r].response_bs[i] = Vector{Float64}(undef, iters) 
+        end
+    end
+    for r in rates
+        for i in 1:nrow(refgrids[r])
+            refgrids[r][i, :response_bs] = [e[i] for e in bstimates[r]]
+        end
+    end
+end
+
+"""
+        bootmargins(
+            vbl, mm, pbt, pbf;
+            resp_var = :response, iters = 1000, minimal = true
+        )
+
+## Description
+
+        jboot(
+            vbl, bimodel, pbs, dats;
+            iters = 1000,
+            invlink = identity,
+            respvar = :response
+        )
 
 ## Description
 
 Input `bimodel` and `parametricbootstrap()` results for each to generate bootstrapped confidence intervals for Youden's J statistic over the margins of a variable of interest `vbl`. `iteration` specifies the number of replications for generating TPR 
 
 """
-function bootmargins(
-    vbl, mm, pbt, pbf, crt, crf;
-    resp_var = :response, iters = 1000, outputall = false,
-    kin = :kin431,
-    manual_typicals = [:age]
+function jboot(
+    vbl, bimodel, pbs, dats;
+    iters = 1000,
+    invlink = identity,
+    confrange = [0.025, 0.975],
+    respvar = :response
 )
 
-    mm = deepcopy(mm);
+    # we will overwrite the model parameters during the bootstrap
+    # (this strategy simplifies prediction and marginal effects calculation)
+    bm = deepcopy(bimodel);
 
-    num_bs = length(pbt.θ);
+    effdict = usualeffects(dats, vbl)
+    refgrids = referencegrid(dats, effdict)
+    apply_referencegrids!(bm, refgrids; invlink)
+    ci!(refgrids)
 
-    # hard-code stratification by kin
-    udict = design_dict([vbl, kin], crt, crf);
-    # vbl, kin, resp_var, :err, :lower, :upper, :verity, :iter 
+    @assert nrow(refgrids[:tpr]) .== nrow(refgrids[:fpr])
 
-    # *2 for verity
-    rtype = Float64; # pred prob from logistic
-    mdf_len = length(udict[vbl]) * length(udict[kin]) * num_bs * 2;
-    iterlen = length(udict[vbl]) * length(udict[kin]) * 2;
-
-    mdf0 = DataFrame(
-        vbl => nonmissingtype(eltype(crt[!, vbl]))[],
-        kin => nonmissingtype(eltype(crt[!, kin]))[],
-        resp_var => rtype[],
-        :err => Float64[],
-        :lower => Float64[],
-        :upper => Float64[],
-        :verity => Bool[],
-        :iter => Int[]
-    );
-
-    mdf = similar(mdf0, mdf_len);
-
-    blocks = [(1:iterlen).+iterlen*(i-1) for i in 1:num_bs];
-
-    # iteration
-    # at each iter, install new parameters for `tpr` and `fpr` models
-    # each i corresponds to a block in blocks
-
-    _assign_margins!(
-        mdf, mm, pbt, pbf, num_bs, blocks, vbl, kin, crt, crf, manual_typicals
-    )
-
-    # reduce to a nice format, partly wide, 
-    mdf_ = @chain mdf begin
-        groupby([kin, :verity, vbl])
-        combine(:response => Ref, renamecols = false)
-        groupby([kin, vbl])
-        combine(:response => Ref, renamecols = false)
+    for r in rates
+        dropmissing!(refgrids[r], [vbl, kin])
     end
 
-    mdf_[!, :peirce] = [fill(NaN, iters) for  _ in eachindex(mdf_.response)];
-    mdf_[!, :accuracy_2d] = [
-        Vector{Point{2, Float64}}(undef, iters) for  _ in eachindex(mdf_.response)
-    ];
-    mdf_[!, :peirce_mean] = fill(NaN, nrow(mdf_));
-    mdf_[!, :peirce_lwr] = fill(NaN, nrow(mdf_));
-    mdf_[!, :peirce_upr] = fill(NaN, nrow(mdf_));
-    mdf_[!, :peirce_err] = fill(NaN, nrow(mdf_));
+    # bstimates corresponds to the response column in each refgrid in refgrids
+    bstimates = _bootstrap_allocate(refgrids, iters, respvar)
+
+    Θ = bootstrapdata(pbs, iters);
+
+    # perform resampling, calculate distribution of marginal effect predictions
+    _assignmargins!(bstimates, refgrids, bm, Θ, iters, invlink, rates);
+
+    # install correct values to bm
+    apply_referencegrids!(bm, refgrids; invlink)
+
+    rg = deepcopy(refgrids);
+    postprocess_bootstrap!(rg, bstimates, iters)
+
+    # combine rate rg data -> rgc
+    for r in rates; sort!(rg[r], [kin, vbl]) end
+
+    # if these are not the same, we cannot lazily combine:
+    @assert(rg[:tpr][!, [kin, vbl]] == rg[:fpr][!, [kin, vbl]])
+
+    # lazily add columns, renaming as appropriate
+    rgc = select(rg[:fpr], Not([:response, :err, :response_bs]))
+    for r in rates rgc[!, r] = rg[r][!, respvar] end
+    for r in rates rgc[!, "err_" * string(r)] = rg[r][!, :err] end
+    for r in rates rgc[!, "ci_" * string(r)] = rg[r][!, :ci] end
+    for r in rates rgc[!, "bs_" * string(r)] = rg[r][!, :response_bs] end
+
+    #=
+    resample to calculate
+       - two-dimensional accuracy scores
+       - j statistic
+    iters times (same as initial bootstrap, for simplicity)
+    =#
+
+    # data structures
+    rgc.bs_accuracy = Vector{Vector{Point2{Float64}}}(undef, nrow(rgc))
+
+    # calculate j statistic
+    rgc[!, :peirce] = rgc[!, :tpr] - rgc[!, :fpr];
+
+    rgc.bs_j = Vector{Vector{Float64}}(undef, nrow(rgc))
     
-    for (k, o) in enumerate(mdf_.response)
-        tpr_, fpr_ = o;
+    for i in eachindex(rgc.bs_accuracy)
+        rgc.bs_accuracy[i] = Vector{Point2}(undef, iters)
+        rgc.bs_j[i] = Vector{Float64}(undef, iters)
+    end
+
+    # resample
+    for (i, (s, v)) in (enumerate∘zip)(rgc.bs_tpr, rgc.bs_fpr)
         for j in 1:iters
-            p1, p2 = rand(tpr_), rand(fpr_)
-            mdf_.peirce[k][j] = p1 - p2 # J = TPR - FPR
-            mdf_.accuracy_2d[k][j] = Point(p2, p1)
+            vr, sr = rand(v), rand(s)
+            rgc.bs_accuracy[i][j] = Point(vr, sr) # (fpr, tpr)
+            rgc.bs_j[i][j] = sr - vr # tpr - fpr
         end
-        mdf_[k, :peirce_mean] = est = mean(mdf_.peirce[k])
-        mdf_[k, :peirce_err] = std(mdf_.peirce[k], corrected = true);
-
-        # https://www.stat.cmu.edu/~ryantibs/advmethods/notes/bootstrap.pdf
-        # basic bootstrap CI
-        q25, q975 = quantile(mdf_.peirce[k], [0.025, 0.975])
-
-        mdf_[k, :peirce_lwr] = 2*est − q975;
-        mdf_[k, :peirce_upr] = 2*est − q25;
     end
 
-    mdf_.cov = [cov(dd) for dd in mdf_[!, :accuracy_2d]];
-
-    return if outputall
-        mdf_
-    else
-        select(mdf_, Not(resp_var))
+    # basic bootstrap CI
+    # https://www.stat.cmu.edu/~ryantibs/advmethods/notes/bootstrap.pdf
+    rgc.ci_j = fill((NaN, NaN), nrow(rgc));
+    rgc.bsinfo_j = Vector{NamedTuple}(undef, nrow(rgc));
+    for (i, (x, θ)) in (enumerate∘zip)(rgc.bs_j, rgc.peirce)
+        qte = (q025, q975) = quantile(x, confrange)
+        rgc.bsinfo_j[i] = (
+            mean = mean(x), std = std(x, corrected = false), ptiles = qte
+        )
+        rgc.ci_j[i] = (2θ - q975, 2θ - q025)
     end
+
+    # confidence ellipse
+    # https://www.stat.cmu.edu/~larry/=stat401/lecture-18.pdf
+    rgc.Σ = [cov(x) for x in rgc.bs_accuracy]
+
+    return rgc
 end
 
-export bootmargins
+export jboot
