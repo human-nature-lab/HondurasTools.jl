0e26748b0cd9bbc2a13385758ab6a134aca03707
emfeltham <eric.feltham@yale.edu>
2024-05-08 12:43:57 -0400

    improve reference grids

--- src/referencegrid.jl
+++ src/referencegrid.jl
@@ -1,13 +1,68 @@
 # referencegrid.jl
 
 """
+        referencegrid(df::BiData, effectsdicts; rates = rates)
+
+## Description
+
+Apply `referencegrid` to a BiData object.
+"""
+function referencegrid(df::BiData, effectsdicts; rates = rates)
+    return (; [r => referencegrid(df[r], effectsdicts[r]) for r in rates]...)
+end
+
+export referencegrid
+
+"""
+        referencegrid(df::AbstractDataFrame, effectsdict)
+
+## Description
+
+Construct a reference grid DataFrame from all possible combinations of the
+input effects dictionary `effectsdict` values.
+"""
+function referencegrid(df::AbstractDataFrame, effectsdict)
+    
+    kys = collect(keys(effectsdict));
+    cp = vec(collect(Iterators.product(values(effectsdict)...)));
+    df = similar(df, length(cp));
+    df = select(df, kys)
+
+    for (i, c) in (enumerate∘eachcol)(df)
+        c .= [e[i] for e in cp]
+    end
+
+    return df
+end
+
+export referencegrid
+
+function apply_referencegrids!(
+    m::EModel, referencegrids;
+    invlink = identity, multithreaded = true
+)
+    if multithreaded
+        #Threads.@threads for r in rates
+        for r in rates
+            effects!(referencegrids[r], m[r]; invlink)
+        end
+    else
+        for r in rates
+            effects!(referencegrids[r], m[r]; invlink)
+        end
+    end
+end
+
+export apply_referencegrids!
+
+"""
         combinerefgrid(rg; rates = rates, kin = kin)
 
 ## Description
 
 Combine separate rate reference grids.
 """
-function combinerefgrid(rg, vbl; rates = rates, kin = kin)
+function combinerefgrid(rg, vbl, respvar; rates = rates, kin = kin)
 
     vx = intersect(names(rg.tpr), names(rg.fpr), [string(vbl), string(kin)])
     rgc = select(rg[:fpr], Not([:response, :err, :ci]))
@@ -25,6 +80,7 @@ export combinerefgrid
 function processrefgrid(
     rg, bimodel, vbl, iters, invlink;
     pbs = nothing,
+    respvar = :response,
     confrange = [0.025, 0.975],
     type = :normal
 )
@@ -34,7 +90,7 @@ function processrefgrid(
             confrange, respvar = :response,
         )
     else
-        combinerefgrid(rg, vbl)
+        combinerefgrid(rg, vbl, respvar)
     end
 
     return disallowmissing!(rg)
